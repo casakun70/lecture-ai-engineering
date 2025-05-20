54597ab8f22e340386497492779c57a2461ea4d4
TSUKASA KOBAYASHI <74399574+casakun70@users.noreply.github.com>
2025-05-21 00:50:42 +0900

    Update black_check.py

--- day5/演習2/black_check.py
+++ day5/演習2/black_check.py
@@ -1,19 +1,18 @@
 # dummy comment to trigger CI
 
 def say_hello(name):
-    print("Hello," + name + "!")  # greet
+    print("Hello," + name       + "!")  # greet
 
 
 def say_hello(name):
-    print("Hello," + name + "!")  # greet
+    print("Hello,"     + name + "   !")    # greet
 
 
 def add(a, b):
     return a + b
 
 
-def add(a, b):
-    return a + b
+def add(a, b):   return a + b
 
 
 def add(a, b):
